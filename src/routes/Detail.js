e510a9eb671e8fae082ddbc7fd56c1a7245c323d
younsoo Mac <moonyoungsu@naver.com>
2024-07-21 20:56:12 +0900

    add useEffect2 homework

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -11,12 +11,17 @@ let YellowBtn = styled.button`
 function Detail(props) {
   let { id } = useParams();
   let [count, setCount] = useState(0);
+  let [num, setNum] = useState("");
 
   useEffect(() => {
     setTimeout(() => {
       document.getElementById("alert").style.display = "none";
     }, 2000);
-  });
+
+    if (isNaN(num) == true) {
+      alert("숫자를 입력해주세요.");
+    }
+  }, [num]);
 
   //TODO: find 함수로 변환해보기
   for (let index in props.shoes.id) {
@@ -24,6 +29,10 @@ function Detail(props) {
       return (id = index);
     }
   }
+
+  const alertClose = (parameter) => {
+    //document.getElementById("alert_input").style.display = parameter;
+  };
 
   return (
     <div className="container">
@@ -43,7 +52,15 @@ function Detail(props) {
             width="100%"
           />
           {/* TODO: useEffectf를 사용하여 숫자가 아닌 값이 입력됐을때  경고창 띄우기*/}
-          <input></input>
+          <input
+            onChange={(e) => {
+              setNum(e.target.value);
+            }}
+          />
+          <div class="alert-box" id="alert_input">
+            <p id="name">알림창 입니다.</p>
+            <button onclick={alertClose("none")}>닫기</button>
+          </div>
           <h4 className="pt-5">{props.shoes[id].title}</h4>
           <p>{props.shoes[id].content}</p>
           <p>{props.shoes[id].price}</p>
